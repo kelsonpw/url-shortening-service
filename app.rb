e3ed6a0d76c69678c3c8ce8a8a9a77da9c4b1e2d
Kelson Warner <kelson@null.net>
2019-08-10 18:16:40 -0700

    add json api routes to sinatra app

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "redis"
+require "json"
 
 redis = Redis.new
 
@@ -20,12 +21,49 @@ post "/" do
   if params[:url] and not params[:url].empty?
     shortcode = random_string 5
     @base_url = request.url + shortcode
-    redis.setnx "links:#{shortcode}", params[:url]
+    redis.setnx("links:#{shortcode}", params[:url])
   end
   erb :index
 end
 
 get "/:shortcode" do
-  @url = redis.get "links:#{params[:shortcode]}"
+  @url = redis.get("links:#{params[:shortcode]}")
   redirect @url || "/"
+end
+
+post "/api/compress" do
+  content_type :json
+  request.body.rewind
+  params = JSON.parse(request.body.read, :symbolize_names => true)
+  if params[:url]
+    shortcode = random_string(5)
+    url = request.base_url + "/" + shortcode
+    redis.setnx("links:#{shortcode}", params[:url])
+    {
+      :shortcode => shortcode,
+      :url => url,
+    }.to_json
+  else
+    {
+      :error => "No URL provided.",
+    }.to_json
+  end
+end
+
+post "/api/expand" do
+  content_type :json
+  request.body.rewind
+  params = JSON.parse(request.body.read, :symbolize_names => true)
+
+  if params[:shortcode]
+    url = redis.get("links:#{params[:shortcode]}")
+    {
+      :url => url,
+      :shortcode => params[:shortcode],
+    }.to_json
+  else
+    {
+      :error => "No shortcode provided.",
+    }.to_json
+  end
 end
